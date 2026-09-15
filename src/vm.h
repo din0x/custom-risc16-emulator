73b110ad1a70b91c438c5f194d58ece7f9365a26
din0x <superrobertxd52@gmail.com>
2026-09-15 21:36:32 +0200

    feat: Update special registers

--- src/vm.h
+++ src/vm.h
@@ -6,9 +6,29 @@
 #include "ram.h"
 
 typedef enum : uint8_t {
-    REG_PC     = 0,
-    REG_SP     = 1,
-    REG_FLAGS  = 2,
+    REG_0      = 0x0,
+    REG_1      = 0x1,
+    REG_2      = 0x2,
+    REG_3      = 0x3,
+    REG_4      = 0x4,
+    REG_5      = 0x5,
+    REG_6      = 0x6,
+    REG_7      = 0x7,
+    REG_8      = 0x8,
+    REG_9      = 0x9,
+    REG_10     = 0xa,
+
+    /* Reserved, may be assigned special meaning in the future */
+    REG_11     = 0xb,
+    REG_12     = 0xc,
+    REG_13     = 0xd,
+    REG_14     = 0xe,
+    REG_15     = 0xf,
+
+    /* Special registers */
+    REG_FLAGS  = REG_13,
+    REG_SP     = REG_14,
+    REG_PC     = REG_15,
 } Reg;
 
 typedef struct Vm {
