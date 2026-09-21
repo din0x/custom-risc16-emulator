d9941f97c241762c99f5dd6f1979e1dd2f862f1b
din0x <superrobertxd52@gmail.com>
2026-09-21 22:34:30 +0200

    fmt: Format .asm files

--- counter.asm
+++ counter.asm
@@ -1,8 +1,8 @@
-mov8 r3 1
-mov8 r4 11
-mov8 r5 1
+    mov8    r3 1
+    mov8    r4 11
+    mov8    r5 1
 loop:
-trap
-add r3 r5
-brlt r3 r4 loop
-halt
+    trap
+    add     r3 r5
+    brlt    r3 r4 loop
+    halt
